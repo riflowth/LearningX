1f1b1f19f4102ad39a1024c85f466568af7d7e0c
riflowth <riflowth@gmail.com>
2019-03-01 21:51:04 +0700

    [Added/Refactored] weightsum function and other stuffs

--- main.py
+++ main.py
@@ -4,12 +4,11 @@
 # 0 Equal to White pixel (empty)
 
 # Model of numbers
-model_0 = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
-model_1 = [[1, 1, 1], [0, 0, 1], [0, 0, 1]]
-modelLists = [model_0, model_1]
+number1 = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+number2 = [[1, 1, 1], [0, 0, 1], [0, 0, 1]]
+modelLists = [number1, number2]
 
-template1 = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-test_template = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+template = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 # -- Functions -- #
 # Print model
@@ -22,8 +21,17 @@ def train(models):
     for buf1 in models:
         for x in range(len(buf1)):
             for y in range(len(buf1[0])):
-                template1[x][y] += buf1[x][y]
-    read(template1)
+                template[x][y] += buf1[x][y]
+    read(template)
+    print(weightsum())
+
+# Calculate weight sum of the template
+def weightsum():
+    buf = 0
+    for powerChild in template:
+        for subChild in powerChild:
+            buf += subChild
+    return buf
 
 # Takenote: 10 = 0, 13 = 1
 
