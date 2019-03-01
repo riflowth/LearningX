134b6593231fdedf0fbd06376b3df07bb1eac26c
riflowth <riflowth@gmail.com>
2019-03-01 21:25:15 +0700

    [Added] read/train function and other stuffs

--- main.py
+++ main.py
@@ -7,3 +7,25 @@
 model_0 = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
 model_1 = [[1, 1, 1], [0, 0, 1], [0, 0, 1]]
 modelLists = [model_0, model_1]
+
+template1 = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+test_template = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# -- Functions -- #
+# Print model
+def read(model):
+    for child in model:
+        print(child)
+
+# Train model (Weight calculate)
+def train(models):
+    for buf1 in models:
+        for x in range(len(buf1)):
+            for y in range(len(buf1[0])):
+                template1[x][y] += buf1[x][y]
+    read(template1)
+
+# Takenote: 10 = 0, 13 = 1
+
+# Main work
+train(modelLists)
