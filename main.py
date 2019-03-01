91a1a7202ddc53ee891df045aef25e5ae8d97bf3
riflowth <riflowth@gmail.com>
2019-03-01 21:03:15 +0700

    [Added] Models

---
+++ main.py
@@ -0,0 +1,9 @@
+# RiFlowTH (CodeR)
+
+# 1 Equal to Black pixel
+# 0 Equal to White pixel (empty)
+
+# Model of numbers
+model_0 = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+model_1 = [[1, 1, 1], [0, 0, 1], [0, 0, 1]]
+modelLists = [model_0, model_1]
